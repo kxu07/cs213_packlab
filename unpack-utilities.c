7ec3c5fd52357c51cf97500389751a99a67ba7f4
kxu07 <117796569+kxu07@users.noreply.github.com>
2026-01-19 13:57:56 -0600

    Update unpack-utilities.c

--- unpack-utilities.c
+++ unpack-utilities.c
@@ -36,7 +36,6 @@ void parse_header(uint8_t* input_data, size_t input_len, packlab_config_t* confi
 	//make sure that the input_len is a valid length
   config->is_valid = false;
 	if (input_len < 20) {
-    printf("line 38\n");
     return;
 	} 
 
@@ -77,12 +76,12 @@ void parse_header(uint8_t* input_data, size_t input_len, packlab_config_t* confi
   // get original length and compressed length
   uint64_t orig_length = 0;
   for (int i=4; i<12; i++) {
-    orig_length += ((uint64_t) input_data[i] << 8*(i-4));
+    orig_length |= ((uint64_t) input_data[i] << 8*(i-4));
   }
   config->orig_data_size = orig_length;
   uint64_t comp_length = 0;
   for (int i=12; i<20; i++) {
-    comp_length += ((uint64_t) input_data[i] << 8*(i-12));
+    comp_length |= ((uint64_t) input_data[i] << 8*(i-12));
   }
   config->data_size = comp_length;
 
@@ -106,7 +105,7 @@ uint16_t calculate_checksum(uint8_t* input_data, size_t input_len) {
   // TODO
   // Calculate a checksum over input_data
   // Return the checksum value
-  uint8_t checksum = 0;
+  uint16_t checksum = 0;
   for (int i=0; i< input_len; i++) {
     checksum += input_data[i];
   }
@@ -142,7 +141,6 @@ void decrypt_data(uint8_t* input_data, size_t input_len,
     printf("Output size too small for decrypt_data\n");
   }
   for (int i=0; i<input_len-1; i+=2) {
-    printf("lfsr_state = %x\n", lfsr_state);
     output_data[i] = lfsr_state ^ input_data[i];
     output_data[i+1] = (lfsr_state>>8) ^ input_data[i+1];
     lfsr_state = lfsr_step(lfsr_state);
@@ -150,7 +148,6 @@ void decrypt_data(uint8_t* input_data, size_t input_len,
   }
   if (input_len%2 == 1) {
     //odd byte 
-    printf("lfsr_state = %x\n", lfsr_state);
     output_data[input_len-1] = lfsr_state^input_data[input_len-1];
   }
   // should be fine -> areas of anxiety (indexing?)
@@ -210,7 +207,34 @@ void join_float_array(uint8_t* input_signfrac, size_t input_len_bytes_signfrac,
   // Combine two streams of bytes, one with signfrac data and one with exp data,
   // into one output stream of floating point data
   // Output bytes are in little-endian order
-
+  size_t total_len = input_len_bytes_signfrac + input_len_bytes_exp;
+  if (output_len_bytes < total_len) {
+    printf("Output length too small for join_float_array\n");
+    return;
+  }
+  size_t signfrac_index = 0;
+  size_t exp_index = 0;
+  size_t output_index = 0;
+  while (true) {
+    //Byte 0 of output
+    output_data[output_index] = input_signfrac[signfrac_index];
+    //Byte 1 of output
+    output_data[output_index+1] = input_signfrac[signfrac_index+1];
+    //Byte 2 of output
+    uint8_t byte2 = input_signfrac[signfrac_index+2] & 0x7F; //lower 7 bits from signfrac
+    byte2 |= (input_exp[exp_index] & 1) << 7; //lowest bit from exp
+    output_data[output_index+2] = byte2;
+    //Byte 3 of output
+    uint8_t byte3 = input_exp[exp_index] >> 1; //upper 7 bits from exp
+    byte3 |= (input_signfrac[signfrac_index+2]) & 0x80; //add sign bit from signfrac
+    output_data[output_index+3] = byte3;
+    signfrac_index += 3;
+    exp_index++;
+    output_index += 4;
+    if (signfrac_index >= input_len_bytes_signfrac || exp_index >= input_len_bytes_exp) {
+      break;
+    }
+  }
 }
 /* End of mandatory implementation. */
 
@@ -228,6 +252,46 @@ void join_float_array_three_stream(uint8_t* input_frac,
   // Combine three streams of bytes, one with frac data, one with exp data,
   // and one with sign data, into one output stream of floating point data
   // Output bytes are in little-endian order
+  size_t num_floats = input_len_bytes_exp; //number of floats is determined by exp length
 
+  for (size_t i = 0; i < num_floats; i++) {
+    //Extract mantissa
+    uint32_t mantissa = 0;
+    size_t mantissa_current_byte = i * 23 / 8;
+    size_t mantissa_starting_bit = i * 23 % 8;
+    uint32_t bitdiff = 8 - mantissa_starting_bit;
+    // If mantissa starts in middle of byte
+    if (mantissa_starting_bit > 0) {
+      uint32_t mask = 0xFF >> mantissa_starting_bit;
+      mantissa |= input_frac[mantissa_current_byte] & mask;
+      mantissa_current_byte++;
+    }
+
+    // Read guaranteed two full bytes
+    for (int j = 0; j < 2; j++) {
+      mantissa |= input_frac[mantissa_current_byte] << (j * 8 + bitdiff);
+      mantissa_current_byte++;
+    }
+
+    // Read remaining bits if any
+    int remaining_bits = 23 - 16 - bitdiff;
+    if (remaining_bits) {
+      mantissa |= input_frac[mantissa_current_byte] >> (8 - remaining_bits) << (16 + bitdiff);
+    }
+
+    // Write to output
+    // Byte 0
+    output_data[i] = mantissa & 0xFF;
+    // Byte 1
+    output_data[i + 1] = (mantissa >> 8) & 0xFF;
+    // Byte 2
+    output_data[i + 2] = mantissa >> 16;
+    output_data[i + 2] |= (input_exp[i] & 1) << 7;
+    // Byte 3
+    output_data[i + 3] = input_exp[i] >> 1;
+    size_t sign_byte = i / 8;
+    int sign_bit = i % 8;
+    output_data[i + 3] |= ((input_sign[sign_byte] >> sign_bit) & 1) << 7;
+  }
 }
 
